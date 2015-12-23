00669a072f11c90b98edac75327283998a170651
paul <paul.bedaride@xilopix.com>
2015-12-23 16:45:51 +0100

    [lint] Auto-correct the code using the 'Style/AlignParameters' rule
    Change-Id: I310010c7e79529950ee81bf02537a8c9bf959d6d

--- lib/xi_image.rb
+++ lib/xi_image.rb
@@ -2,7 +2,7 @@ require 'log4r'
 require 'log4r/yamlconfigurator'
 
 Log4r::YamlConfigurator.load_yaml_file(File.expand_path('./log4r.yml',
-                                                        File.dirname(__FILE__)))
+  File.dirname(__FILE__)))
 
 module XiImage
   Logger = Log4r::Logger['xi_nlp']
